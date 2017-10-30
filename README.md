c52a9190e76eaf7840fea724c560cd2a78b8040f
bausano <gtasampserver@seznam.cz>
2017-10-30 11:26:09 +0000

    Init

---
+++ README.md
@@ -0,0 +1,1 @@
+# Herocard Client
---
+++ src/herocard/client/Client.java
@@ -0,0 +1,14 @@
+package herocard.client;
+
+/**
+ * A GUI client for HeroCard game.
+ * 
+ * @author michael
+ */
+public class Client {
+
+    public static void main(String[] args) {
+        
+    }
+    
+}
